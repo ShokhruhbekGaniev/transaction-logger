56a2179fdb1d0af0e81d02489656ae404820eb92
Shokhrukhbek Ganiev <shohchik9999@gmail.com>
2026-01-13 13:17:43 +0500

    init: go module and health endpoint

---
+++ cmd/api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_ = json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	addr := ":8080"
+	log.Printf("starting server on %s", addr)
+
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ShokhruhbekGaniev/transaction-logger
+
+go 1.25.5
